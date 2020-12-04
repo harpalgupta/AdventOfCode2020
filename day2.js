69207c0c439598f441991d9a83bf532cb3735210
Harpal Gupta <harpal.gupta@gmail.com>
2020-12-04 08:50:59 +0000

    day part1 complete

---
+++ day2.js
@@ -0,0 +1,48 @@
+let input = [];
+
+const fs = require("fs");
+try {
+  input = fs.readFileSync("./day2Input.txt", "utf8").split("\n");
+} catch (err) {
+  console.error(err);
+}
+
+let regex = /^(\d+)-(\d+) ([a-z]): (\w+)/;
+
+var validPassword =0;
+
+input.forEach(line => {
+    let matches = line.match(regex);
+
+    let atLeast = matches[1];
+    let atMost = matches[2];
+
+    let letter = matches[3];
+
+    let password = matches[4];
+
+
+    // console.log(letter);
+    let regexp = new RegExp (`${letter}`,'gm')
+     let occurances = 0;
+    // let occurances =( regexp.exec(password))||[];
+
+
+    password.split("").forEach(currentLetter => {
+      if(currentLetter.toLowerCase() == letter.toLowerCase()){
+        occurances++;
+      }
+    });
+
+    if( occurances >= atLeast && occurances <= atMost){
+
+        validPassword++;
+         console.log( `good password at least ${atLeast} at most ${atMost} for letter ${letter} count: ${occurances} password: ${password} `)
+    }
+    else{
+
+    }
+
+});
+
+console.log("valid Passwords",validPassword)
