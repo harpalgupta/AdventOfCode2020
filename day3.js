2046a757da2accf347db2b279d65c74eec3b11d0
Harpal Gupta <harpal.gupta@gmail.com>
2020-12-05 09:08:32 +0000

    day3 part 2 complete

--- day3.js
+++ day3.js
@@ -7,33 +7,49 @@ try {
   console.error(err);
 }
 
-let right=3;
-let down=1;
-
-let treeCount=0;
-let currentPosition=0;
 
 
-var patternLength= input.length * right ;
-console.log("patternLenght",patternLength);
+const GetTreeCount = (right, down, input) => {
+  let treeCount = 0;
+  let currentPosition = 0;
 
-let timesToRepeat = Math.ceil(patternLength / input[0].length)
+  var patternLength = input.length * right;
+  console.log("patternLenght", patternLength);
 
-input.forEach((line) => {
- 
-let repeatedLine="";
+  let timesToRepeat = Math.ceil(patternLength / input[0].length);
 
-  for (let index = 0; index < timesToRepeat; index++) {
-    repeatedLine+=line
-  }
-  console.log("currentpos", repeatedLine.split("")[currentPosition]);
-  if(repeatedLine.split("")[currentPosition]==="#"){
+  input.forEach((line, lineIndex) => {
+    if (lineIndex % down === 0) {
+      console.log(lineIndex, down);
 
-    treeCount++
-  }
+      let repeatedLine = "";
 
-  currentPosition+=(right);
+      for (let index = 0; index < timesToRepeat; index++) {
+        repeatedLine += line;
+      }
+      console.log("currentpos", repeatedLine.split("")[currentPosition]);
+      if (repeatedLine.split("")[currentPosition] === "#") {
+        treeCount++;
+      }
 
+      currentPosition += right;
+    }
+    //else skip
+  });
+  return treeCount;
+};
+
+const instructPart1 = {right:3,down:1};
+
+const instructPart2 = [{right:1,down:1},{right:3,down:1},{right:5,down:1},{right:7,down:1},{right:1,down:2}]
+var results = [];
+
+
+instructPart2.forEach(instr => {
+  results.push(GetTreeCount(instr.right, instr.down, input))
 });
 
-console.log(treeCount);
+var multipliedResults= results.reduce((prevValue,curValue) =>  prevValue * curValue,1)
+
+console.log("part1 answer ", GetTreeCount(instructPart1.right,instructPart1.down,input));
+console.log("part2 answer ",multipliedResults);
