74d3031de058242ae5c9e603a3b2a1f6948c27d2
Harpal Gupta <harpal.gupta@gmail.com>
2020-12-16 21:10:49 +0000

    day part 2 complete

--- day4.js
+++ day4.js
@@ -7,103 +7,107 @@ try {
   console.error(err);
 }
 
-
-const GetJsonPassport=(passport)=>{
-  var tempPass = passport.replace(/\s/gm,"\",\"");
-  tempPass = tempPass.replace(/:/gm,"\":\"");
+const GetJsonPassport = (passport) => {
+  var tempPass = passport.replace(/\s/gm, '","');
+  tempPass = tempPass.replace(/:/gm, '":"');
   var json = `{ \"${tempPass}\" }`;
   return json;
-}
+};
 
+const IsPassportCorrect = (passport) => {
+  var keysNeeded = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
 
-const IsPassportCorrect=(passport)=>{
+  var failedReason = "";
 
-  var keysNeeded=["byr","iyr","eyr","hgt","hcl","ecl","pid"]
-
-  var failedReason ="";
-
-  keysNeeded.forEach(key => {
-    // console.log("checking key",key)
-    if(!(passport[key])) {
-      failedReason+= (key+" not in passport")
-      console.log("missing key",key)
-    } 
-
+  keysNeeded.forEach((key) => {
+    if (!passport[key]) {
+      failedReason += key + " not in passport";
+      console.log("missing key", key);
+    }
   });
 
-  if(failedReason===""){
-    return true
+  if (failedReason === "") {
+    return true;
   }
-  
-  return false
-  
-}
+
+  return false;
+};
 
 
-const CheckAllPassports=(passportInput) =>{
+const YearCheck = (year, min, max) => {
 
-  let validPassportCount =0;
-  let invalidPassportCount =0;
+  return year.toString().length === 4 && year * 1 >= min && year * 1 <= max;
+};
 
-  passportInput.forEach(passport => {
+const HeightCheck = (height) => {
+
+  if (new RegExp(/\d+cm/).test(height)) {
+    console.log(height.slice(0, -2), "cm");
+    return (height.slice(0, -2) * 1 >= 150) & (height.slice(0, -2) * 1 <= 193);
+  } else if (new RegExp(/\d+in/).test(height)) {
+    return (height.slice(0, -2) * 1 >= 59) & (height.slice(0, -2) * 1 <= 76);
+  }
+};
+
+const HairColourCheck = (hairColour) => {
+  return new RegExp(/#[0-9a-f]{6}\b/gm).test(hairColour);
+};
+
+const EyeColourCheck = (eyeColour) => {
+  return new RegExp(/amb\b|blu\b|brn\b|gry\b|grn\b|hzl\b|oth\b/).test(
+    eyeColour
+  );
+};
+
+const PassporIDCheck = (pid) => {
+  parseInt(pid)
+  return new RegExp(/^\d{9}\b/).test(pid);
+};
+
+const PassportDataCheck = (passport) => {
+  if (
+    YearCheck(passport["byr"], 1920, 2002) &&
+    YearCheck(passport["iyr"], 2010, 2020) &&
+    YearCheck(passport["eyr"], 2020, 2030) &&
+    HeightCheck(passport["hgt"]) &&
+    HairColourCheck(passport["hcl"]) &&
+    EyeColourCheck(passport["ecl"]) &&
+    PassporIDCheck(passport["pid"])
+  )
+    return true;
+  else {
+    console.log(passport);
+    return false;
+  }
+};
+
+const CheckAllPassports = (passportInput) => {
+  let validPassportCount = 0;
+  let invalidPassportCount = 0;
+
+  let validPassportPart2 = 0;
+  passportInput.forEach((passport) => {
+
+    
     var passport = JSON.parse(GetJsonPassport(passport));
 
-    if(IsPassportCorrect(passport)){
-      validPassportCount++
+    if (IsPassportCorrect(passport)) {
+      validPassportCount++;
+
+      if (PassportDataCheck(passport)) {
+        validPassportPart2++;
+        console.log(passport["pid"])
+      }
+    } else {
+      invalidPassportCount++;
     }
-    else{
-      invalidPassportCount++
-    }
-});
+  });
 
-console.log(`Valid Passports: ${validPassportCount} Invalid Passports: ${invalidPassportCount}`)
-
-const YearCheck=(year,min,max)=>{
-return year.length()===4 && year>=min && year<=max
-
-}
-
-const HeightCheck = (height)=>{
-  if (height.search("cm")){
-    return height>=150 & height<=193
-  }
-  else{
-    return height>=59 & height<=76
-
-  }
-
-}
-
-const PassportDataCheck =(passport)=>{
-
-
-  YearCheck(passport["byr"],1920,2020);
-  YearCheck(passport["iyr"],2010,2020);
-  YearCheck(passport["eyr"],2020,2030);
-  HeightCheck(passport["hgt"])
-
-
- 
-}
-
-//Part2 checks...
-//length,min,max
-// byr 4,1920,2002.
-// iyr 4,2010,2020.
-// eyr 4,2020,2030.
-
-// hgt (Height) - a number followed by either cm or in:
-// If cm, the number must be at least 150 and at most 193.
-// If in, the number must be at least 59 and at most 76.
-
-// hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-// ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-
-// pid (Passport ID) - a nine-digit number, including leading zeroes.
-// cid (Country ID) - ignored, missing or not.
-
-}
+  console.log(
+    `Valid Passports: ${validPassportCount} Invalid Passports: ${invalidPassportCount}`
+  );
+  console.log(`valid Passports part 2: ${validPassportPart2}`);
+};
 
 CheckAllPassports(input);
-
 
